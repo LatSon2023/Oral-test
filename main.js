c81c5ce242ed95fcd426579fa4be65b575a5be08
LatSon2023 <2023newbie>
2023-02-25 13:29:08 +0700

    second commit

--- main.js
+++ main.js
@@ -1,105 +1,80 @@
-const root = document.querySelector('#root')
-const heading = document.querySelector('#heading')
-const container = document.querySelector('#container')
-const name = 'Duc'
-const date = new Date()
-const url = 'https://frcz3.sse.codesandbox.io/jobs'
-const limit = '?_page=1&_limit=10'
-const limit2 = '?_page=2&_limit=10'
-const prev = document.querySelector('.prev')
-const next = document.querySelector('.next')
-const input = document.querySelector('.input')
-const btn = document.querySelector('.search')
+const $ = document.querySelector.bind(document)
 
-heading.innerHTML = `
+let name = 'Vũ Ngọc Đức'
+const date = new Date()
+
+$('#heading').innerHTML = `
     <h1>Module 1 Technical Interview</h1>
     <p>Learner name: ${name}</p>
-    <p>Date: ${date.getDay()}</p>
+    <p>Date: ${date.getDate()}</p>
 `
 
-// const getData = async () => {
-//     try {
-//         const response = await fetch(url)
-//         if (response.ok) {
-//             const data = await response.json()
-//             return data
-//         }
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+const URL = `https://frcz3.sse.codesandbox.io/jobs`
 
-const getData2 = async (expand) => {
+const getData = async (info) => {
+    const getUrl = (info) => {
+        if (!info) 
+            return `${URL}?_page=1&_limit=10`
+        if (typeof info === 'number') 
+            return `${URL}?_page=${info}&_limit=10`
+        if (typeof info === 'string') 
+            return `${URL}?q=${info}`
+    }
     try {
-        const response = await fetch(`${url}${expand}`)
-        if (response.ok) {
-            const data = await response.json()
-            return data
-        }   
+        const response = await fetch(getUrl(info))
+            if (response.ok) {
+                const data = await response.json()
+                return data
+            }
     } catch (error) {
         console.log(error)
         return []
     }
 }
 
-const renderData = async (expand) => {
-    const dataTenTitles = await getData2(expand)
-    try {
-        dataTenTitles.forEach((data, index) => {
+const renderData = async (info) => {
+    const dataTenTitles = await getData(info)
+    const renderList = (list) => {
+        list.forEach((data, index) => {
             const title = document.createElement('h1')
             title.innerHTML = `
             ${index + 1}. ${data.title}
             `
-            container.appendChild(title)
+            $('#container').appendChild(title)
         })
-    } catch (error) {
-        console.log(error)
     }
-}
-
-const renderData2 = async (expand) => {
-    const dataTenTitles = await getData2(expand)
-    container.innerHTML = ''
     try {
-        dataTenTitles.forEach((data, index) => {
-            const title = document.createElement('h1')
-            title.innerHTML = `
-            ${index + 1}. ${data.title}
-            `
-            container.appendChild(title)
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
-renderData(limit)
-
-prev.addEventListener('click', () => {
-    renderData2(limit)
-})
-next.addEventListener('click', () => {
-    renderData2(limit2)
-})
-
-const handleSearch = async () => {
-    const inputValue = input.value
-    const expand = `?q=${inputValue}`
-    try {
-        const response = await fetch(`${url}${expand}`)
-        container.innerHTML = ''
-        if (response.ok) {
-            const data = await response.json()
-            data.forEach((value) => {
-                const title = document.createElement('h1')
-                title.innerHTML = `
-                    ${index + 1}. ${data.title}
-                `
-            container.appendChild(title)
-            })
+        if ($('#container').innerHTML === '') {
+            renderList(dataTenTitles)
+        } else {
+            $('#container').innerHTML = ''
+            renderList(dataTenTitles)
         }
     } catch (error) {
         console.log(error)
     }
 }
+renderData()
 
-btn.addEventListener('click', handleSearch)
+let num = 1
+$('.page').innerHTML = `Page: ${num}`
+$('.prev-btn').addEventListener('click', () => {
+    if (num > 1) 
+        num--
+    $('#container').style.overflowY = 'hidden'
+    $('.page').innerHTML = `Page: ${num}`
+    renderData(num)
+})
+
+$('.next-btn').addEventListener('click', () => {
+    num++
+    $('#container').style.overflowY = 'hidden'
+    $('.page').innerHTML = `Page: ${num}`
+    renderData(num)
+})
+
+$('.search-btn').addEventListener('click',() => {
+    $('.page').innerHTML = ''
+    $('#container').style.overflowY = 'scroll'
+    renderData($('.input').value)
+})
